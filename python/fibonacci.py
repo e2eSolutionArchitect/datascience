0c8b241e32fe9a992cab92ab13d67e726fdc4168
The Solution Architect <62712515+TheSolutionArchitect@users.noreply.github.com>
2024-03-29 16:58:09 -0400

    Update fibonacci.py

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,26 +1,22 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-# We are creating an array containing n = 10 elements 
-# for getting the first 10 Fibonacci numbers 
-a = np.arange(1, 11) 
-lengthA = len(a) 
+input = np.arange(1,12)
+output = numpy.empty(11, dtype=object)
 
-# splitting of terms for easiness 
-sqrtFive = np.sqrt(5) 
-alpha = (1 + sqrtFive) / 2
-beta = (1 - sqrtFive) / 2
+def mm_fib(n):
+    return (numpy.matrix([[2,1],[1,1]])**(n//2))[0,(n+1)%2]
 
-# Implementation of formula 
-# np.rint is used for rounding off to an integer 
-Fn = np.rint(((alpha ** a) - (beta ** a)) / (sqrtFive)) 
-print("The first {} numbers of Fibonacci series are {} . ".format(lengthA, Fn)) 
+for i in range(11):
+    output[i]= mm_fib(i)
+
+print(output)
 
 # red for numpy.log() 
-plt.plot(Fn, a, 
+plt.plot(input, output, 
 		color = 'red', marker = "o") 
 		
-plt.title("numpy.log()") 
-plt.xlabel("out_array") 
-plt.ylabel("in_array") 
+plt.title("Fibonacci numbers") 
+plt.xlabel("Input") 
+plt.ylabel("Output") 
 plt.show() 
