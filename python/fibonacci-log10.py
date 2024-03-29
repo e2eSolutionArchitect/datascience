5a2126d8d92ebaae4e04d66bf09842bda21ed835
The Solution Architect <62712515+TheSolutionArchitect@users.noreply.github.com>
2024-03-29 18:08:05 -0400

    Update fibonacci-log10.py

--- python/fibonacci-log10.py
+++ python/fibonacci-log10.py
@@ -7,7 +7,9 @@ import math
 
 input_number=100
 a = np.arange(1, input_number) 
+output_log5 = np.arange(1, input_number) 
 output_log10 = np.arange(1, input_number) 
+output_log20 = np.arange(1, input_number) 
 lengthA = len(a) 
 
 # splitting of terms for easiness 
@@ -18,22 +20,26 @@ beta = (1 - sqrtFive) / 2
 # Implementation of formula 
 # np.rint is used for rounding off to an integer 
 Fn = np.rint(((alpha ** a) - (beta ** a)) / (sqrtFive)) 
-print('Fn values',Fn)
+#print('Fn values',Fn)
 
 m=0
 
 for k in Fn:
-    output_log10[m]=math.log10(k)
-    print('Log base 10 of {} is {}'.format(k, math.log10(k)))
+    output_log5[m]=math.log(k,5)
+    output_log10[m]=math.log(k,10)
+    output_log20[m]=math.log(k,20)
+    #print('Log base 10 of {} is {}'.format(k, math.log10(k)))
     m=m+1
 
 #print(output_log10)
 
-print("The first {} numbers of Fibonacci series are {} . ".format(lengthA, Fn)) 
+#print("The first {} numbers of Fibonacci series are {} . ".format(lengthA, Fn)) 
 
 # plt.plot(a, Fn,color = 'red', marker = "o") 
 
-plt.plot(a, output_log10,color = 'red', marker = "o") 
+plt.plot(a, output_log5,color = 'red', marker = "o") 
+plt.plot(a, output_log10,color = 'blue', marker = "o") 
+plt.plot(a, output_log20,color = 'green', marker = "o") 
 		
 plt.title("Fibonacci") 
 plt.xlabel("Input") 
